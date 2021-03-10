ffb92b6f1e0bfb79462c64a55b4c170e83902718
Mehmet Yurtar <mehmet.yurtar@cimri.com>
2021-03-10 18:38:10 +0300

    new version

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
 function zoomin(){
     //$(".zoom-in").css("transform", "scale(1.5)");
-    setTimeout(() => {
-        $(".zoom-in").css("background-image", "url('./images/mainbg2.jpg')");
+    let loop = 1;
+    setInterval(() => {
+        loop += 1;
+        if(loop === 4){
+            loop = 1
+        }
+        $(".zoom-in").css("background-image", `url('./images/mainbg${loop}.jpg')`);
     }, 4000)
-    setTimeout(() => {
-        $(".zoom-in").css("background-image", "url('./images/mainbg3.jpg')");
-    }, 9000)
-
 }
 
 function zoominSecond(){
     //$(".zoom-in").css("transform", "scale(1.5)");
-    setTimeout(() => {
+    let loop = 15;
+    setInterval(() => {
+        console.log("loop", loop)
+        loop += 1;
+        if(loop === 19){
+            loop = 16
+        }
         $(".active-second-header").removeClass("active-second-header");
-        $(".zoom-in-second h2").eq(1).addClass("active-second-header")
-        $(".zoom-in-second").css("background-image", "url('./images/arkaplan_giris-16.jpg')");
+        $(".zoom-in-second h2").eq(loop -15).addClass("active-second-header")
+        $(".zoom-in-second").css("background-image", `url('./images/arkaplan_giris-${loop}.jpg')`);
     }, 4000)
-    setTimeout(() => {
-        $(".active-second-header").removeClass("active-second-header");
-        $(".zoom-in-second h2").eq(2).addClass("active-second-header")
-        $(".zoom-in-second").css("background-image", "url('./images/arkaplan_giris-17.jpg')");
-    }, 9000);
-    setTimeout(() => {
-        $(".active-second-header").removeClass("active-second-header");
-        $(".zoom-in-second h2").eq(3).addClass("active-second-header")
-        $(".zoom-in-second").css("background-image", "url('./images/arkaplan_giris-18.jpg')");
-    }, 14000)
 }
 
 zoomin();
